2100ad8271cc15d3911c7c602833080334ec1330
Adrián Bükfa <bukfa.adrian@gmail.com>
2022-11-23 16:38:25 +0100

    #8 Implement endpoint to get transaction by id

--- src/main/java/com/codecool/transactstat/controller/WalletController.java
+++ src/main/java/com/codecool/transactstat/controller/WalletController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 public class WalletController {
@@ -25,5 +26,11 @@ public class WalletController {
     @PostMapping("/api/wallet/transactions")
     public void addTransaction(@RequestBody Transaction transaction){
         walletService.addTransaction(transaction);
+    }
+
+    @GetMapping("api/transactions/{transactionId}")
+    public Transaction getById(@PathVariable(name = "transactionId") String id){
+        UUID uuid = UUID.fromString(id);
+        return walletService.getTransaction(uuid);
     }
 }
