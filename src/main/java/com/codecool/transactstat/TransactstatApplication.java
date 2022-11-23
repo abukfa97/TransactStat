c8a85d0d370560c6e91a2611d565fd69bcf8197d
Adrián Bükfa <bukfa.adrian@gmail.com>
2022-11-23 11:11:18 +0100

    Set up the Maven  project

---
+++ src/main/java/com/codecool/transactstat/TransactstatApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.transactstat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactstatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactstatApplication.class, args);
+	}
+
+}
