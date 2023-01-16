2322a9306eb8f1f8c8116695d3fdd9f7cef392ec
Adrián Bükfa <bukfa.adrian@gmail.com>
2023-01-16 15:26:31 +0100

    Separate Wallet and TransactionController

--- src/main/java/com/codecool/transactstat/controller/WalletController.java
+++ src/main/java/com/codecool/transactstat/controller/WalletController.java
@@ -21,60 +21,7 @@ public class WalletController {
         this.transactionService = transactionService;
     }
 
-    @GetMapping("/api/wallet/transactions")
-    public List<Transaction> getTransactions(){
-        return transactionService.getTransactions();
-    }
-
-    @GetMapping("/api/wallet/transactions/date/{date}")
-    public List<Transaction> getTransactionsByDate(@PathVariable String date){
-        var localDate = LocalDate.parse(date);
-        return transactionService.getTransactionsByDate(localDate);
-    }
-
-    @PostMapping("/api/wallet/transactions")
-    public void addTransaction(@RequestBody Transaction transaction){
-        transactionService.addTransaction(transaction);
-    }
-
-    @GetMapping("/api/wallet/transactions/{transactionId}")
-    public Transaction getById(@PathVariable(name = "transactionId") String id){
-        UUID uuid = UUID.fromString(id);
-        return transactionService.getTransaction(uuid);
-    }
-
-    @PutMapping("/api/wallet/transactions/{transactionId}")
-    public void updateTransaction(@PathVariable(name = "transactionId") UUID id,@RequestBody Transaction transaction){
-        transactionService.updateTransaction(transaction, id);
-    }
-
-    @DeleteMapping("/api/wallet/transactions/{transactionId}")
-    public void deleteTransaction(@PathVariable(name = "transactionId") UUID id){
-        transactionService.deleteTransaction(id);
-    }
-
-    @GetMapping("/api/wallet/transactions/expenses")
-    public List<Transaction> getExpenses(){
-        return transactionService.getExpenses();
-    }
-
-    @GetMapping("/api/wallet/transactions/incomes")
-    public List<Transaction> getIncomes(){
-        return transactionService.getIncomes();
-    }
-
-    @GetMapping("/api/wallet/transactions/biggest-transaction")
-    public Transaction getBiggestTransaction(){
-        return transactionService.getBiggestTransaction();
-    }
-
     @GetMapping("api/wallet/current-balance")
     public BigDecimal getCurrentBalance(){return transactionService.getCurrentBalance();}
-
-    @ExceptionHandler(TransactionNotFoundException.class)
-    public ResponseEntity<?> handleMissingTransaction(){
-        return ResponseEntity.notFound().build();
-    }
-
 
 }
