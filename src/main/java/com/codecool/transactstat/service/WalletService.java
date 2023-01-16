6c0330d37ac2b3459bdf1cbc1d866c12d8be6d68
Adrián Bükfa <bukfa.adrian@gmail.com>
2023-01-16 17:42:16 +0100

    Implement endpoint for get balance by walletId

---
+++ src/main/java/com/codecool/transactstat/service/WalletService.java
@@ -0,0 +1,29 @@
+package com.codecool.transactstat.service;
+
+import com.codecool.transactstat.model.Wallet;
+import com.codecool.transactstat.persistent.WalletRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Service
+public class WalletService {
+
+    private final WalletRepository walletRepository;
+
+    @Autowired
+    public WalletService(WalletRepository walletRepository) {
+        this.walletRepository = walletRepository;
+    }
+
+    public Wallet getWalletById(UUID walletId){
+        return walletRepository.getReferenceById(walletId);
+    }
+
+
+    public BigDecimal getBalanceByWalletId(UUID walletId){
+        return  getWalletById(walletId).getBalance();
+    }
+}
