339b7050a32ba1066752ec0e9142ad282d733949
Adrián Bükfa <bukfa.adrian@gmail.com>
2023-01-17 15:18:25 +0100

    #57 implement endpoint for register new user

--- src/main/java/com/codecool/transactstat/controller/UserController.java
+++ src/main/java/com/codecool/transactstat/controller/UserController.java
@@ -3,13 +3,10 @@ package com.codecool.transactstat.controller;
 import com.codecool.transactstat.model.User;
 import com.codecool.transactstat.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/api/users/{userId}")
 public class UserController {
 
     private final UserService userService;
@@ -19,8 +16,13 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("/{userId}")
+    @GetMapping
     public User getUserById(@PathVariable Long userId){
         return userService.getUserById(userId);
+    }
+
+    @PostMapping
+    public void registerNewUser(@PathVariable Long userId, @RequestBody User user){
+        userService.updateUserById(userId, user);
     }
 }
