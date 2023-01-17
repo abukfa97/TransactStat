7d707e935ee0f369a56bb3c165e15a4452236476
Adrián Bükfa <bukfa.adrian@gmail.com>
2023-01-17 16:26:12 +0100

    #57 Implement endpoint for get wallets by userId

--- src/main/java/com/codecool/transactstat/controller/WalletController.java
+++ src/main/java/com/codecool/transactstat/controller/WalletController.java
@@ -1,6 +1,7 @@
 package com.codecool.transactstat.controller;
 
 import com.codecool.transactstat.model.Transaction;
+import com.codecool.transactstat.model.Wallet;
 import com.codecool.transactstat.service.TransactionService;
 import com.codecool.transactstat.service.WalletService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +26,11 @@ public class WalletController {
     @GetMapping("api/{walletId}/balance")
     public BigDecimal getCurrentBalance(@PathVariable Long walletId){
         return walletService.getBalanceByWalletId(walletId);
+    }
+
+    @GetMapping("api/wallets/{userId}")
+    public List<Wallet> getWalletsByUserId(@PathVariable Long userId){
+        return walletService.getWalletsByUserId(userId);
     }
 
 }
--- src/main/java/com/codecool/transactstat/service/WalletService.java
+++ src/main/java/com/codecool/transactstat/service/WalletService.java
@@ -1,20 +1,30 @@
 package com.codecool.transactstat.service;
 
+import com.codecool.transactstat.model.User;
 import com.codecool.transactstat.model.Wallet;
+import com.codecool.transactstat.persistent.UserRepository;
 import com.codecool.transactstat.persistent.WalletRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Service
 public class WalletService {
 
     private final WalletRepository walletRepository;
+    private final UserRepository userRepository;
 
     @Autowired
-    public WalletService(WalletRepository walletRepository) {
+    public WalletService(WalletRepository walletRepository, UserRepository userRepository) {
         this.walletRepository = walletRepository;
+        this.userRepository = userRepository;
+    }
+
+    public List<Wallet> getWalletsByUserId(Long userId){
+        User user = userRepository.getReferenceById(userId);
+        return walletRepository.getWalletsByUser(user);
     }
 
     public Wallet getWalletById(Long walletId){
