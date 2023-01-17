718da2c966a777832d96ab091039a93f279cc891
Adrián Bükfa <bukfa.adrian@gmail.com>
2023-01-17 14:43:25 +0100

    Implement basic CRUD methods in UserService

--- src/main/java/com/codecool/transactstat/service/UserService.java
+++ src/main/java/com/codecool/transactstat/service/UserService.java
@@ -1,5 +1,6 @@
 package com.codecool.transactstat.service;
 
+import com.codecool.transactstat.model.User;
 import com.codecool.transactstat.persistent.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,5 +13,24 @@ public class UserService {
     @Autowired
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    public User getUserById(Long id){
+        return userRepository.getReferenceById(id);
+    }
+
+    public void addUser(User user){
+        userRepository.save(user);
+    }
+
+    public void deleteUser(User user){
+        userRepository.delete(user);
+    }
+
+    public void updateUserById(Long id, User user){
+        User userToUpdate = getUserById(id);
+        userToUpdate.setFirstName(user.getFirstName());
+        userToUpdate.setLastName(user.getLastName());
+        userToUpdate.setWallets(user.getWallets());
     }
 }
