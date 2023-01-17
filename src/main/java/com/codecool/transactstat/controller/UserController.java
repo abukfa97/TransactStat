b998e2c51f9665bf15c20fad9c97f46c920ca21a
Adrián Bükfa <bukfa.adrian@gmail.com>
2023-01-17 14:54:15 +0100

    #57 Implement enpoint for get user by id

---
+++ src/main/java/com/codecool/transactstat/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.codecool.transactstat.controller;
+
+import com.codecool.transactstat.model.User;
+import com.codecool.transactstat.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/{userId}")
+    public User getUserById(@PathVariable Long userId){
+        return userService.getUserById(userId);
+    }
+}
