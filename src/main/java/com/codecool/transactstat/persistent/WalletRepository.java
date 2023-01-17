25b4c261b3d50b2df8e0d360c25769f287cee6c5
Adrián Bükfa <bukfa.adrian@gmail.com>
2023-01-17 12:57:45 +0100

    Refactor type of Wallet id to Long

--- src/main/java/com/codecool/transactstat/persistent/WalletRepository.java
+++ src/main/java/com/codecool/transactstat/persistent/WalletRepository.java
@@ -3,7 +3,5 @@ package com.codecool.transactstat.persistent;
 import com.codecool.transactstat.model.Wallet;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.UUID;
-
-public interface WalletRepository extends JpaRepository<Wallet,UUID> {
+public interface WalletRepository extends JpaRepository<Wallet,Long> {
 }
