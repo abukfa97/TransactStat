918cd13ef32768890874f852f5959e20ff1537f0
Adrián Bükfa <bukfa.adrian@gmail.com>
2023-01-18 16:48:01 +0100

    #70 Add existsAppUserByUserName method to UserRepository

--- src/main/java/com/codecool/transactstat/persistent/UserRepository.java
+++ src/main/java/com/codecool/transactstat/persistent/UserRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<AppUser,Long> {
+    boolean existsAppUserByUserName(String userName);
 }
