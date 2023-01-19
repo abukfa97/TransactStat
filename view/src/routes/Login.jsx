ca9b9434064c944245aeac4111ea754e3aa2f15a
domonkos97 <szabo.domonkos97@gmail.com>
2023-01-19 14:15:03 +0100

    handle user login verification

--- view/src/routes/Login.jsx
+++ view/src/routes/Login.jsx
@@ -1,14 +1,38 @@
+import {useEffect, useState} from "react";
+import { browserHistory } from 'react-router';
 const Login = ({  }) => {
+    const [userName, setUserName] = useState("")
+    const [error, setError] = useState("")
+    const handleStatus = async(e) => {
+        e.preventDefault();
+        const user = {"userName": userName};
+        let response = await fetch('/api/wallet/transactions',
+            {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify(user)
+            } )
+        if (response.status === 403) {
+            browserHistory.push("/");
+        } else if (response.status === 202) {
+            setError("There is something wrong!")
+        }
+            //reponse.status ==403 or 202
+
+    }
+
     return (
+
         <div className="form">
-            <form>
+            <p className="error">{error}</p>
+            <form onSubmit={handleStatus}>
                 <div className="input-container">
                     <label>Username </label>
-                    <input type="text" name="uname" required />
+                    <input type="text" name="uname" required value={userName} onChange={(e) => {setUserName(e.target.value)}}/>
                 </div>
                 <div className="input-container">
                     <label>Password </label>
-                    <input type="password" name="password" required />
+                    <input type="password" name="password" required value={}/>
                 </div>
                 <div className="button-container">
                     <input type="submit" />
@@ -17,5 +41,7 @@ const Login = ({  }) => {
         </div>
     )
 }
+
+// fetch /auth -> if Response: ACCEPTED -> redirect to homepage
 
 export default Login
