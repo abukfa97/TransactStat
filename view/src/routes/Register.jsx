f0e32edb4500b9f64737e3ce95eb2f25f1e9bf17
domonkos97 <szabo.domonkos97@gmail.com>
2023-01-19 12:54:25 +0100

    add register form

--- view/src/routes/Register.jsx
+++ view/src/routes/Register.jsx
@@ -1,7 +1,24 @@
 const Register = () => {
-    return (
-        <div>
 
+    return (
+        <div className="form">
+            <form>
+                <div className="input-container">
+                    <label>Email </label>
+                    <input type="email" name="email" required/>
+                </div>
+                <div className="input-container">
+                    <label>Username </label>
+                    <input type="text" name="uname" required/>
+                </div>
+                <div className="input-container">
+                    <label>Password </label>
+                    <input type="password" name="password" required/>
+                </div>
+                <div className="button-container">
+                    <input type="submit"/>
+                </div>
+            </form>
         </div>
     )
 }
