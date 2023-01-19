148a3fe32d125bbfcfe55d41ddfb83a334329aee
Adrián Bükfa <bukfa.adrian@gmail.com>
2023-01-19 14:54:37 +0100

    #79 Implement createTransactionDTO

--- src/main/java/com/codecool/transactstat/service/TransactionService.java
+++ src/main/java/com/codecool/transactstat/service/TransactionService.java
@@ -31,6 +31,18 @@ public class TransactionService {
         return transactionRepository.getTransactionsByWallet(walletRepository.getReferenceById(walletId));
     }
 
+    public TransactionDTO createTransactionDTO(Transaction transaction){
+        TransactionDTO dto = new TransactionDTO();
+        dto.setId(transaction.getId());
+        dto.setTitle(transaction.getTitle());
+        dto.setWalletId(transaction.getWallet().getId());
+        dto.setAmount(transaction.getAmount());
+        dto.setDateOfTransaction(transaction.getDateOfTransaction());
+        dto.setPaymentType(transaction.getPaymentType());
+        dto.setTransactionCategory(transaction.getTransactionCategory());
+        return dto;
+    }
+
     public Transaction getTransaction(Long id) {
         Optional<Transaction> transaction = Optional.of(transactionRepository.getReferenceById(id));
         return transaction.orElseThrow(TransactionNotFoundException::new);
