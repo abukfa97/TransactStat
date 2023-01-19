04b7cc8fa114c29c56a9b55e19c7957eae0de508
domonkos97 <szabo.domonkos97@gmail.com>
2023-01-19 12:27:21 +0100

    create form for login

--- view/src/routes/Login.jsx
+++ view/src/routes/Login.jsx
@@ -1,6 +1,19 @@
 const Login = ({  }) => {
     return (
-        <div>
+        <div className="form">
+            <form>
+                <div className="input-container">
+                    <label>Username </label>
+                    <input type="text" name="uname" required />
+                </div>
+                <div className="input-container">
+                    <label>Password </label>
+                    <input type="password" name="password" required />
+                </div>
+                <div className="button-container">
+                    <input type="submit" />
+                </div>
+            </form>
         </div>
     )
 }
