705c54e554aa57c09d5d7d9c945dcd349925d089
domonkos97 <szabo.domonkos97@gmail.com>
2023-01-23 09:07:32 +0100

    create simple signup form

--- view/src/routes/Login.jsx
+++ view/src/routes/Login.jsx
@@ -1,8 +1,9 @@
-import {useEffect, useState} from "react";
-import {Link, Redirect} from 'react-router-dom';
-import {Alert, FormControl, FormGroup, Input, InputLabel, Paper, Typography} from "@mui/material";
-import {useHistory} from "react-router-dom";
-import Button from "@mui/material/Button";
+import {useState} from "react";
+import {Alert, Typography} from "@mui/material";
+import {Link, useHistory} from "react-router-dom";
+import GoogleIcon from '@mui/icons-material/Google';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 const Login = () => {
     const [userName, setUserName] = useState("")
@@ -29,24 +30,48 @@ const Login = () => {
 
 
     return (
-        <div className="form">
+        <div>
             <div className="toolbar-height"></div>
-            {errorDisplay}
-            <form onSubmit={handleStatus}>
-                <div className="input-container">
-                    <label>Username </label>
-                    <input type="text" name="uname" required value={userName} onChange={(e) => {setUserName(e.target.value)}}/>
-                </div>
-                <div className="input-container">
-                    <label>Password </label>
-                    <input type="password" name="password" required/>
-                </div>
-                <p>If you are not registered, click <Link to="/register">here</Link></p>
-                <div className="button-container">
-                    <input value="Login" type="submit" />
-                </div>
-            </form>
+            <div className='container-login'>
+                <form onSubmit={handleStatus()}></form>
+                <h4 style={{color: 'white', margin: '100px auto 50px auto'}}>Sign In</h4>
+                    <div className="socials">
+                        <Link to='google.com' className='social-button'><GoogleIcon></GoogleIcon></Link>
+                        <Link to='google.com' className='social-button'><FacebookIcon></FacebookIcon></Link>
+                        <Link to='google.com' className='social-button'><GitHubIcon></GitHubIcon></Link>
+                    </div>
+                    <div className='email-sign-in-form'>
+                        <Typography>
+                            Sign In With Email
+                        </Typography>
+                        <input type="text" placeholder='Name' className='input-field'/>
+                        <input type="email" placeholder='Email'  className='input-field'/>
+                        <input type="password" placeholder='Password'  className='input-field'/>
+                        <Link className='forgot-your-password'>Forgot your password?</Link>
+                        <input type="submit" value='Sign In' className='submit-button'/>
+                    </div>
+                <div className='container-right'></div>
+            </div>
         </div>
+
+        // <div className="form">
+        //
+        //     {errorDisplay}
+        //     <form onSubmit={handleStatus}>
+        //         <div className="input-container">
+        //             <label>Username </label>
+        //             <input type="text" name="uname" required value={userName} onChange={(e) => {setUserName(e.target.value)}}/>
+        //         </div>
+        //         <div className="input-container">
+        //             <label>Password </label>
+        //             <input type="password" name="password" required/>
+        //         </div>
+        //         <p>If you are not registered, click <Link to="/register">here</Link></p>
+        //         <div className="button-container">
+        //             <input value="Login" type="submit" />
+        //         </div>
+        //     </form>
+        // </div>
     )
 }
 
