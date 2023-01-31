79d5b29668f483134e53bc3374ce9a92a50c0068
Adrián Bükfa <bukfa.adrian@gmail.com>
2023-01-31 15:34:13 +0100

    #99 Override loadUserByUsername method from UserDetailsService

--- src/main/java/com/codecool/transactstat/service/UserService.java
+++ src/main/java/com/codecool/transactstat/service/UserService.java
@@ -4,21 +4,32 @@ import com.codecool.transactstat.model.UserEntity;
 import com.codecool.transactstat.model.dto.DtoFactory;
 import com.codecool.transactstat.model.dto.UserDTO;
 import com.codecool.transactstat.persistent.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
+    private final PasswordEncoder passwordEncoder;
+
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public UserDTO getUserById(Long id){
@@ -27,8 +38,12 @@ public class UserService {
     }
 
     public void addUser(UserEntity userEntity){
-        boolean isExisting = userRepository.existsAppUserByUserName(userEntity.getUserName());
-        if (!isExisting) userRepository.save(userEntity);
+        boolean isExisting = userRepository.existsUserEntityByUserName(userEntity.getUserName());
+        if (!isExisting) {
+            log.info("***** CREATED USER {} *****", userEntity.getUserName());
+            userEntity.setPassword(passwordEncoder.encode(userEntity.getPassword()));
+            userRepository.save(userEntity);
+        }
     }
 
     public void deleteUserById(Long userId){
@@ -49,8 +64,18 @@ public class UserService {
                .collect(Collectors.toList());
     }
 
-    public Optional<Long> authenticate(UserDTO user){
-        UserEntity searched = userRepository.getAppUserByUserName(user.getUserName());
+    /*public Optional<Long> authenticate(UserDTO user){
+        UserEntity searched = userRepository.getUserEntityByUserName(user.getUserName()).
+                orElseThrow(() -> new UsernameNotFoundException(user.getUserName()));
         return Optional.ofNullable(searched).map(UserEntity::getId);
+    } */
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity user = userRepository.getUserEntityByUserName(username)
+                .orElseThrow(() -> new UsernameNotFoundException(username));
+        List<SimpleGrantedAuthority> roles = new ArrayList<>();
+        roles.add(new SimpleGrantedAuthority(user.getRole().name()));
+        return new User(user.getUserName(),user.getPassword(), roles);
     }
 }
